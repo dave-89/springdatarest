d81aa48f3cfae7216172e0f1c0c19ff826e64341
drdave89 <drdave89@yahoo.com>
2019-01-22 23:58:09 +0000

    working filter

--- src/main/java/com/davide/demo/springdatarestmongo/configuration/FilterConfiguration.java
+++ src/main/java/com/davide/demo/springdatarestmongo/configuration/FilterConfiguration.java
@@ -1,14 +1,18 @@
 package com.davide.demo.springdatarestmongo.configuration;
 
-import com.davide.demo.springdatarestmongo.filters.AddingHTTPSFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
 public class FilterConfiguration {
 
+
     @Bean
-    public AddingHTTPSFilter addingHTTPSFilter() {
-        return new AddingHTTPSFilter();
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        final FilterRegistrationBean<ForwardedHeaderFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new ForwardedHeaderFilter());
+        return bean;
     }
 }
